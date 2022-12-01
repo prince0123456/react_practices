bc8e4135252c11ee09321781c68819da0212d691
Prince Gupta <princegupta.pg46@gmail.com>
2022-12-01 12:46:35 +0530

    first_test_commit

---
+++ src/components/FirstTest.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const FirstTest = () => {
+  return (
+    <>
+        <h1> Netflix</h1>
+        <div>
+            <p> Paragraph</p>
+        </div>
+        <ol>
+            <li>Netflix1</li>
+            <li>Netflix1</li>
+            <li>Netflix1</li>
+        </ol>
+
+    </> )
+}
